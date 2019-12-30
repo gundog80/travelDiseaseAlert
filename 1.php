bed0e3881907fcbd58ca24389b5c25394dfb9fbe
gundog80 <gundog80@gmail.com>
2019-12-30 16:41:31 +0800

    1230-a

--- 1.php
+++ 1.php
@@ -1,3 +1,8 @@
+<?php
+	include_once "country.php";
+
+?>
+
 <!DOCTYPE html>
 <html lang="zh-TW">
 <head>
@@ -11,20 +16,83 @@
 	include_once "./world.svg";
 	?>
 
-<script src="./jquery-3.4.1.min.js"></script>
 </body>
+<script src="./jquery-3.4.1.min.js"></script>
 <script>
 	//svg滑入滑出
+	let paths =$('path')
+
 	$('path').mouseenter((event)=>{
+		$('.svg').append(`<use id="upup" xlink:href="#${$(event.target).attr('id')}"/>`)
 		$(event.target).attr('transform','translate(-5, -5)')
 		$(event.target).attr('filter',"url(#filter1)")
+		$(event.target).appendTo('svg')
 	} )
 
 	$('path').mouseleave((event)=>{
 		$(event.target).attr('transform','translate(0, 0)')
 		$(event.target).removeAttr('filter')
+		$('#upup').remove()
 	})
 	//svg滑入滑出end
+
+	let news=""
+$(function(){
+	getOpenData();
+	// temp()
+	setTimeout(	temp,3000)
+	setTimeout(	getRow,3000)
+
+});
+
+	let getOpenData=function(){
+		//取得opendata
+		let url="https://www.cdc.gov.tw/CountryEpidLevel/ExportJSON"
+		$.post("./api.php",{url},function(data){
+			data=data.substring(0,data.indexOf(']')+1)
+			news=JSON.parse(data)
+			// console.log(news[0].Source);
+			// console.log(news[0]);
+		})
+	}
+	let temp= function(){
+		// 測式用
+			console.log(news[0]);
+	}
+
+	let try1=function(){
+		// 測試用，開div
+   console.log("hi")
+	}
+
+	let getRow=function(){
+		// 取得單行資料(若為多國則再分)
+	news.forEach(function temp1(row){
+	// let row = news[0];
+	// console.log(row)
+		// row=JSON.parse(row);
+	severityLevel=row.severity_level;
+	alertDisease=row.alert_disease;
+	countries=row.description;
+	if (countries==""){
+	// 	country=row.areaDesc
+	// 	console.log(country)
+	}else{
+		console.log(countries)
+		setTimeout(
+		countries.split(","),
+		1000
+		)
+		// countries.forEach(function temp2(country){
+		console.log(countries)
+
+		// }
+		// )
+	}
+
+	})
+	}
+
 
 </script>
 </html>
